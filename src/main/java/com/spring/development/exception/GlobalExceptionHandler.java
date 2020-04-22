0e16b4e4bce906b97e69e22b8eb1a05553bdc247
XuZhenkui <987856377@qq.com>
2020-04-22 10:18:58 +0800

    添加全局异常处理,将异常仅暴露在服务器内部, 对用户友好显示

---
+++ src/main/java/com/spring/development/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.spring.development.exception;
+
+import com.spring.development.aspect.LogAspect;
+import com.spring.development.common.ResultCode;
+import com.spring.development.common.ResultJson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Description
+ * @Project development
+ * @Package com.spring.development.exception
+ * @Author xuzhenkui
+ * @Date 2020/4/22 9:44
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(LogAspect.class);
+
+    /**
+     * 处理空指针的异常
+     * @param request
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value =NullPointerException.class)
+    public ResultJson exceptionHandler(HttpServletRequest request, NullPointerException e){
+        logger.error("发生空指针异常！原因是:",e);
+        return ResultJson.failure(ResultCode.SERVICE_UNAVAILABLE);
+    }
+
+
+    /**
+     * 处理其他异常
+     * @param request
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value =Exception.class)
+    public ResultJson exceptionHandler(HttpServletRequest request, Exception e){
+        logger.error("未知异常！原因是:",e);
+        return ResultJson.failure(ResultCode.INTERNAL_SERVER_ERROR);
+    }
+}
