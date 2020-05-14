fbc67386e7bc83b14b70d6ce0288fc39c3f6a699
XuZhenkui <987856377@qq.com>
2020-05-14 18:00:48 +0800

    修改线程池: 核心线程数量, 最大线程池数量

--- src/main/java/com/spring/development/util/ThreadPoolExecutorFactory.java
+++ src/main/java/com/spring/development/util/ThreadPoolExecutorFactory.java
@@ -12,11 +12,16 @@ import java.util.concurrent.*;
  * @Date 2020/5/10 8:24
  */
 public class ThreadPoolExecutorFactory {
-    private final static int CORE_POOL_SIZE = 3;
 
-    private final static int MAXIMUM_POOL_SIZE = 6;
+    //    核心线程数量: 取 CPU 核心数 / 2 + 1 个
+    private final static int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors() % 2 +1;
 
-    private final static long KEEP_ALIVE_TIME = 3 * 60;
+    //    最大线程池数量:
+//    1. CPU 密集型任务配置尽可能少的线程数量, 一般公式: CPU 核心数 + 1个线程的线程池
+//    2. IO 密集型: 1. 配置尽可能多的线程: CPU核心数 * 2;  2. CPU核心数 / 1 - 阻塞系数(0.8 - 0.9) 例: 8 / (1-0.9)
+    private final static int MAXIMUM_POOL_SIZE = Runtime.getRuntime().availableProcessors() + 1;
+
+    private final static long KEEP_ALIVE_TIME = 30L;
 
     private final static TimeUnit TIME_UNIT = TimeUnit.SECONDS;
 
